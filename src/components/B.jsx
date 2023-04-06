7352b03b2e4122802a65e6af6bc8e501d65e891f
Crypto-kiki <ejo2123@naver.com>
2023-04-06 09:30:47 +0900

    final

---
+++ src/components/B.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import C from "./C";
+
+function B() {
+  return (
+    <>
+      <Box w={100} h={100} bgColor={"green.200"}>
+        B
+      </Box>
+      <C />
+    </>
+  );
+}
+
+export default B;
